c6c2162cdb7a14b3515ec99e5aea06a447b37ec6
Hassan Al-khalifah and Luke Vandekieft <hassan@>
2018-09-25 13:47:03 -0700

    Set up images.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import './scss/input.scss';
 import $ from "jquery";
+
+$(document).ready(function() {
+  var counter = 0;
+  $(window).scroll(function() {
+    console.log(counter);
+    counter ++;
+    $("#test").text(counter);
+  });
+});
