061866abba741e7acf473970a519554be32472e6
Hassan Al-khalifah and Luke Vandekieft <luke@>
2018-09-27 10:22:25 -0700

    Switch scroll strategy from counter to scrolltop.

--- src/index.js
+++ src/index.js
@@ -3,49 +3,52 @@ import $ from "jquery";
 
 $(document).ready(function() {
 
-  var counter = 0;
-  var contentCounter = 0;
-  var maxScrollOut = 21;
-  var firstDisappear = 13;
-  var lastActiveCall = 0;
-  $(".test").bind('mousewheel', function(event) {
-    console.log(counter);
-    var currentCall = new Date().getTime();
-    var scrollDelay = currentCall - lastActiveCall;
-    if (scrollDelay > 250 || scrollDelay < 210) {
-      lastActiveCall = currentCall;
-      if((event.originalEvent.wheelDelta / 120 > 0) && (counter >= 1)) {
-        counter --;
-        console.log("scroll up, mouse down (t)");
-        $("#steps").css("transform", "translateX(" + (counter*45) + "px) scale(" + (1+(counter/20)) + ")");
-        $("#steps").css("opacity" , 1-(counter/firstDisappear));
-        $("#mountain").css("transform", "translateX(-" + (counter*35) + "px) scale(" + (1+(counter/20)) + ")");
-        $("#mountain").css("opacity" , 1-(counter/firstDisappear));
-        $("#background").css("transform", "translateX(" + (counter*1) + "px) scale(" + (1+(counter/60)) + ")");
-        // $("#background").css("opacity" , 1-(counter/maxScrollOut));
 
-      } else if ((event.originalEvent.wheelDelta / 120) < 0) {
-        if(counter === 20) {
-          console.log("here");
-          $(".content").css("display", "block");
-          $(".test").css("display", "none");
-        } else {
-          console.log("scroll down, mouse up (t)");
-          $("#steps").css("transform" , "translateX(" + (counter*45) + "px) scale(" + (1+(counter/20)) + ")");
-          $("#steps").css("opacity" , 1-(counter/firstDisappear));
-          $("#mountain").css("transform", "translateX(-" + (counter*35) + "px) scale(" + (1+(counter/20)) + ")");
-          $("#mountain").css("opacity" , 1-(counter/firstDisappear));
-          $("#background").css("transform", "translateX(" + (counter*1) + "px) scale(" + (1+(counter/60)) + ")");
-          // $("#background").css("opacity" , 1-(counter/maxScrollOut));
-          counter ++;
-        }
-      }
-      $(".content").scrollTop(0);
-    } else {
-      console.log(scrollDelay + "zero");
-    }
-  });
+// COUNTER SCROLL ANIMATION FUNCTION
+  // var counter = 0;
+  // var contentCounter = 0;
+  // var maxScrollOut = 21;
+  // var firstDisappear = 13;
+  // var lastActiveCall = 0;
+  // $(".test").bind('mousewheel', function(event) {
+  //   console.log(counter);
+  //   var currentCall = new Date().getTime();
+  //   var scrollDelay = currentCall - lastActiveCall;
+  //   if (scrollDelay > 250 || scrollDelay < 210) {
+  //     lastActiveCall = currentCall;
+  //     if((event.originalEvent.wheelDelta / 120 > 0) && (counter >= 1)) {
+  //       counter --;
+  //       console.log("scroll up, mouse down (t)");
+  //       $("#steps").css("transform", "translateX(" + (counter*45) + "px) scale(" + (1+(counter/20)) + ")");
+  //       $("#steps").css("opacity" , 1-(counter/firstDisappear));
+  //       $("#mountain").css("transform", "translateX(-" + (counter*35) + "px) scale(" + (1+(counter/20)) + ")");
+  //       $("#mountain").css("opacity" , 1-(counter/firstDisappear));
+  //       $("#background").css("transform", "translateX(" + (counter*1) + "px) scale(" + (1+(counter/60)) + ")");
+  //
+  //
+  //     } else if ((event.originalEvent.wheelDelta / 120) < 0) {
+  //       if(counter === 20) {
+  //         console.log("here");
+  //         $(".content").css("display", "block");
+  //         $(".test").css("display", "none");
+  //       } else {
+  //         console.log("scroll down, mouse up (t)");
+  //         $("#steps").css("transform" , "translateX(" + (counter*45) + "px) scale(" + (1+(counter/20)) + ")");
+  //         $("#steps").css("opacity" , 1-(counter/firstDisappear));
+  //         $("#mountain").css("transform", "translateX(-" + (counter*35) + "px) scale(" + (1+(counter/20)) + ")");
+  //         $("#mountain").css("opacity" , 1-(counter/firstDisappear));
+  //         $("#background").css("transform", "translateX(" + (counter*1) + "px) scale(" + (1+(counter/60)) + ")");
+  //
+  //         counter ++;
+  //       }
+  //     }
+  //     $(".content").scrollTop(0);
+  //   } else {
+  //     console.log(scrollDelay + "zero");
+  //   }
+  // });
 
+// WHEEL DELTA CALCULATED NORMAL SCROLL
   // $(".content").bind('mousewheel', function(event) {
   //   if(event.originalEvent.wheelDelta / 120 > 0) {
   //     if (contentCounter < -.2) {
@@ -74,35 +77,68 @@ $(document).ready(function() {
   //   }
   // });
 
+// SCROLL TOP WINDOW SWITCH
+  // $(window).bind('mousewheel', function(event) {
+  //
+  //   if(event.originalEvent.wheelDelta / 120 > 0) {
+  //     if($(window).scrollTop() == 0) {
+  //       $(".content").css("display", "none");
+  //       $(".test").css("display", "flex");
+  //     }
+  //      contentCounter --;
+  //   } else if (event.originalEvent.wheelDelta / 120 < 0) {
+  //     if($(window).scrollTop() + $(window).height() !== $(document).height()) {
+  //        contentCounter ++;
+  //     }
+  //   }
+  // });
 
 
-  $(window).bind('mousewheel', function(event) {
 
-    if(event.originalEvent.wheelDelta / 120 > 0) {
-      if($(window).scrollTop() == 0) {
-        $(".content").css("display", "none");
-        $(".test").css("display", "flex");
-      }
-      // contentCounter --;
-    } else if (event.originalEvent.wheelDelta / 120 < 0) {
-      if($(window).scrollTop() + $(window).height() !== $(document).height()) {
-        // contentCounter ++;
-      }
-    }
-  });
-
+var iScrollPos = 0;
+var counter = 0;
+$("#parent-container").scroll(function() {
+  var iCurScrollPos = $(this).scrollTop();
+  if(iCurScrollPos > iScrollPos) {
+    // Scrolling Down
+  } else {
+    // Scrolling Up
+  }
+  iScrollPos = iCurScrollPos;
+  var windowHeight = $("#parent-container").height();
+  var scrollPercent = iCurScrollPos / (5000 - windowHeight);
+  console.log(windowHeight + "  " + scrollPercent);
 });
 
 
-// var allowScroll = "yes";
-// trigger function() {
-//   setTimeout(function () { allowa; }, 5)
-//   delay .1 second: allowScroll = "no";
-//   delay .4 second: allowScroll = "yes";
-// }
+
+
+// $("#steps").css("transform" , "translateX(" + (counter*45) + "px) scale(" + (1+(counter/20)) + ")");
+// $("#steps").css("opacity" , 1-(counter/firstDisappear));
+// $("#mountain").css("transform", "translateX(-" + (counter*35) + "px) scale(" + (1+(counter/20)) + ")");
+// $("#mountain").css("opacity" , 1-(counter/firstDisappear));
+// $("#background").css("transform", "translateX(" + (counter*1) + "px) scale(" + (1+(counter/60)) + ")");
 //
-// if (delayScroll === "yes") {
-//   REGULAR FUNCTION
-// } else {
-//   NOTHING
-// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
